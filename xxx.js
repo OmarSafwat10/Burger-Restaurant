21e3e20859f5de16f33577893edd7655b14be262
OmarSafwat10 <145147162+OmarSafwat10@users.noreply.github.com>
2023-09-15 19:44:44 +0300

    Add files via upload

---
+++ xxx.js
@@ -0,0 +1,10 @@
+// let names = ['ahmed' , 'yasser' , 'mobarak' , 'ahmed'];
+
+// let x = names.includes('ahmed' , 2);
+
+// document.write(x);
+
+
+// for(let x = 0 ; x < colors.length; x++){
+//     document.write(colors[x] = + '<br>')
+// }
